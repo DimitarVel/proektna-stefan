477686f88b50753ff1baa94e56ec5ec7fd1b615e
Dimo <dimitardev1@gmail.com>
2020-03-27 19:38:11 +0100

    Update blogger.sql

--- blogger.sql
+++ blogger.sql
@@ -35,18 +35,6 @@ CREATE TABLE `posts` (
   `post_body` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`post_id`, `creator_id`, `post_title`, `post_body`) VALUES
-(1, 1, 'How i created this website in under 5 hours', 'I have a lot of experience which i gathered during the past 2 weeks. While i was in quarantine i was working routhlessly to develop the Advanced School System for CodeCup 2020 and i still have a lont way to go so i decided to take my mind off of it so i can return better and stronger to face the next challenge (frontend development) easier.'),
-(2, 1, 'Another Post', 'ndship contrasted solicitude insipidity in introduced literature it. He seemed denote except as oppose do spring my. Between any may mention evening age shortly can ability regular. He shortly sixteen of colonel colonel evening cordial to. Although jointure an my of mistress servants am weddings. Age why the therefore education unfeeling for arranging. Above again money own scale maids ham least led. Returned settling produced strongly ecstatic use yourself way. Repulsive extremity enjoyment she perceived nor. \r\n'),
-(3, 4, 'New Post', 'post body'),
-(4, 4, 'Another post for testing', 'Post Body'),
-(6, 4, 'Third post for testing', 'Post body for the third post.'),
-(7, 5, 'Global research on coronavirus disease (COVID-19)', 'WHO is gathering the latest scientific findings and knowledge on coronavirus disease (COVID-19) and compiling it in a database. We update the database daily from searches of bibliographic databases, hand searches of the table of contents of relevant journals, and the addition of other relevant scientific articles that come to our attention. The entries in the database may not be exhaustive and new research will be added regularly.\n\nCoronavirus disease (COVID-19) is an infectious disease caused by a new virus.\n\nThe disease causes respiratory illness (like the flu) with symptoms such as a cough, fever, and in more severe cases, difficulty breathing. You can protect yourself by washing your hands frequently, avoiding touching your face, and avoiding close contact (1 meter or 3 feet) with people who are unwell.\n\nThe entries in the database may not be exhaustive and new research will be added regularly.Coronavirus disease (COVID-19) is an infectious disease caused by a new virus.\n\nYou can protect yourself by washing your hands frequently, avoiding touching your face, and avoiding close contact (1 meter or 3 feet) with people who are unwell.');
-
 -- --------------------------------------------------------
 
 --
@@ -59,14 +47,6 @@ CREATE TABLE `users` (
   `password` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
-(1, 'Stefan', 'stefanpass'),
-(4, 'Dimitar', 'dimopass'),
-(5, 'New User', 'passfornewuser');
 
 --
 -- Indexes for dumped tables
